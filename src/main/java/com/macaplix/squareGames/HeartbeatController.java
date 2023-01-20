44015fa8f629162ebf2983bc0cd2945422a5448f
PierreBoue <119578551+PierreBoue@users.noreply.github.com>
2023-01-20 12:09:52 +0100

    lunch time !

--- src/main/java/com/macaplix/squareGames/HeartbeatController.java
+++ src/main/java/com/macaplix/squareGames/HeartbeatController.java
@@ -12,12 +12,19 @@ public class HeartbeatController {
     private HeartbeatSensor heartbeatSensor;
     @Autowired
     private GameCatalog gamecatalog;
+    @Autowired
+    private GameCreator gameCreator;
     @GetMapping("/gamelist")
     Collection<String> getGameList()
     {
         return gamecatalog.getGameIdentifiers();
     }
-
+    @GetMapping("/creategame")
+    String creategame( int gameIndex, int playerCount, int boardSize)
+    {
+        return gameCreator.createGame(gameIndex, playerCount, boardSize );
+        //return playerCount + " joueur" + ((playerCount>1)?"s":"") + " size " + boardSize;
+    }
     @GetMapping("/heartbeat")
     int getHeartbeat()
     {
