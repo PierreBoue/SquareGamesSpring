cc9bcfc87e465b72b3defa5b945b3fe205f12f0f
Pierre Boué <37743737+Macaplix@users.noreply.github.com>
2023-02-05 22:07:09 +0100

    bootstrap login interface

--- src/main/resources/public/api/public/js/login.js
+++ src/main/resources/public/api/public/js/login.js
@@ -12,14 +12,26 @@ function sendUserData()
 }
 function tokenReceive( jsn )
 {
-    tokenDTO = JSON.parse(jsn);
+    const tokenDTO = JSON.parse(jsn);
     document.getElementById("tokenView").value = tokenDTO["token"];
-    navigator.clipboard.writeText("Bearer" + tokenDTO["token"]);
-            .then(() => {
+    /*
+    navigator.clipboard.writeText("Bearer" + tokenDTO["token"]).then(() => {
               alert("Token writen to pateboard:\n" + tokenDTO["token"]);
             })
             .catch(err => {
               alert('Error in copying text: ', err);
             });
-
+            */
+    //copyToken( tokenDTO["token"]);
+}
+function copyToken( token )
+{
+    if ( token == null ) token = document.getElementById("tokenView").value;
+    navigator.clipboard.writeText("Bearer " + token).then(() => {
+              alert("Token writen to pateboard:\n" + token);
+            })
+            .catch(err => {
+              alert('Error in copying text: ', err);
+            });
+    
 }
