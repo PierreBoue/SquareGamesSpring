6a98afea943071f7888b88a1db391fa98bf18ebb
Pierre Boué <37743737+Macaplix@users.noreply.github.com>
2023-02-24 21:37:39 +0100

    board finally square !

--- src/main/resources/public/square-games/js/sqgboard.js
+++ src/main/resources/public/square-games/js/sqgboard.js
@@ -8,7 +8,7 @@ function displayBoard( size )
        htmlout += "     <tr class='boardcell'>\n";
         for ( let x =0; x < size; x++)
         {
-            htmlout += "     <td align='center' width='24' height='24' id='bc" + x + "-" + y + "' onclick='selectCell( this );'>\n";
+            htmlout += "     <td align='center' width='26' height='26' id='bc" + x + "-" + y + "' onclick='selectCell( this );'>\n";
             htmlout += "      &nbsp;\n";
             htmlout += "     </td>\n";
         }
@@ -18,12 +18,12 @@ function displayBoard( size )
     const boardesk =  document.getElementById("boarddesk");
     //const  boardcard =  document.getElementById("boardcard");
     const boardiv =document.getElementById("boarddiv");
-    let width = 42 * ( size + 2 );
-    boardiv.style.width = width + "px;";
-    boardiv.style.height = width + "px;";
-    width += 40;
-    boardesk.style.width = width + "px;";
-    boardesk.style.height = width + "px;";
+//    let width = 42 * ( size + 2 );
+//    boardiv.style.width = width + "px;";
+//    boardiv.style.height = width + "px;";
+//    width += 40;
+//    boardesk.style.width = width + "px;";
+//    boardesk.style.height = width + "px;";
    // console.log("displayBoard " + size);
     //boardcard.style.width = width + "px;";
     //boardcard.style.height = width + "px;";
