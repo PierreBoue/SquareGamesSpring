337f3c497f7d71a9d2255b4f0d20c4503fdbab12
Pierre Boué <37743737+Macaplix@users.noreply.github.com>
2023-03-09 22:25:35 +0100

    added test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
 curl -X POST -H "Content-Type: application/json" http://localhost:8081/api/public/mockstat
+curl -X POST -H "Content-Type: application/json" http://macaplix.games/api/public/mockstat
